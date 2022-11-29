ebaee5e8fe7f0d243d96652d158d731037e18274
qhpike <qhpike@gmail.com>
2022-11-29 17:56:21 +0800

    侧边栏调整

--- src/Layout/components/SideBar/SidebarItem.vue
+++ src/Layout/components/SideBar/SidebarItem.vue
@@ -1,16 +1,37 @@
 <template>
-    <div v-if="!item.hidden">
-      <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
-        <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
-          <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-            <item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)"   :title="onlyOneChild.meta.title" />
-          </el-menu-item>
-        </app-link>
-      </template>
-  
-      <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
+  <div v-if="!item.hidden">
+    <template
+      v-if="
+        hasOneShowingChild(item.children, item) &&
+        (!onlyOneChild.children || onlyOneChild.noShowingChildren) &&
+        !item.alwaysShow
+      "
+    >
+      <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
+        <el-menu-item
+          :index="resolvePath(onlyOneChild.path)"
+          :class="{ 'submenu-title-noDropdown': !isNest }"
+        >
+          <item
+            :icon="onlyOneChild.meta.icon || (item.meta && item.meta.icon)"
+            :title="onlyOneChild.meta.title"
+          />
+        </el-menu-item>
+      </app-link>
+    </template>
+
+    <el-submenu
+      v-else
+      ref="subMenu"
+      :index="resolvePath(item.path)"
+      popper-append-to-body
+    >
       <template slot="title">
-        <item v-if="item.meta"  :icon="item.meta && item.meta.icon" :title="item.meta.title" />
+        <item
+          v-if="item.meta"
+          :icon="item.meta && item.meta.icon"
+          :title="item.meta.title"
+        />
       </template>
       <sidebar-item
         v-for="child in item.children"
@@ -21,71 +42,70 @@
         class="nest-menu"
       />
     </el-submenu>
-    </div>
-  </template>
+  </div>
+</template>
 
 
 <script>
-import path from 'path'
-import { isExternal } from '@/utils/validate'
-import Item from './Item.vue'
-import AppLink from './Link'
+import path from "path";
+import { isExternal } from "@/utils/validate";
+import Item from "./Item.vue";
+import AppLink from "./Link";
 
 export default {
-    name: 'SidebarItem',
-    components: { Item, AppLink },
-    props: {
-        item: {
-            type: Object,
-            required: true
-        },
-        isNest: {
-            type: Boolean,
-            default: false
-        },
-        basePath: {
-            type: String,
-            default: ''
-        }
+  name: "SidebarItem",
+  components: { Item, AppLink },
+  props: {
+    item: {
+      type: Object,
+      required: true,
     },
-    data() {
-        this.onlyOneChild = null
-        return {}
+    isNest: {
+      type: Boolean,
+      default: false,
     },
-    methods: {
-        hasOneShowingChild(children = [], parent) {
-            const showingChildren = children.filter(item => {
-                if (item.hidden) {
-                    return false
-                } else {
-                    this.onlyOneChild = item
-                    return true
-                }
-            })
-
-            if (showingChildren.length === 1) {
-                return true
-            }
-
-            if (showingChildren.length === 0) {
-                this.onlyOneChild = { ... parent, path: '', noShowingChildren: true }
-                return true
-            }
-
-            return false
-        },
-        resolvePath(routePath) {
-            if (isExternal(routePath)) {
-                return routePath
-            }
-            if (isExternal(this.basePath)) {
-                return this.basePath
-            }
-            return path.resolve(this.basePath,routePath)
+    basePath: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    this.onlyOneChild = null;
+    return {};
+  },
+  methods: {
+    hasOneShowingChild(children = [], parent) {
+      const showingChildren = children.filter((item) => {
+        if (item.hidden) {
+          return false;
+        } else {
+          this.onlyOneChild = item;
+          return true;
         }
-    }
-}
+      });
+
+      if (showingChildren.length === 1) {
+        return true;
+      }
+
+      if (showingChildren.length === 0) {
+        this.onlyOneChild = { ...parent, path: "", noShowingChildren: true };
+        return true;
+      }
+
+      return false;
+    },
+    resolvePath(routePath) {
+      if (isExternal(routePath)) {
+        return routePath;
+      }
+      if (isExternal(this.basePath)) {
+        return this.basePath;
+      }
+      return path.resolve(this.basePath, routePath);
+    },
+  },
+};
 </script>
 <style scoped>
-
 </style>
