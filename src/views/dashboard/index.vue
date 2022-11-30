b30fc2066f6587c0fc1ef3f5fa17b7f4619fa19e
qhpike <qhpike@gmail.com>
2022-11-30 16:53:12 +0800

    水波纹组件及放大镜

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,7 +4,7 @@
  * @Description: 
 -->
 <template>
-    <div class="app-container">
+    <!-- <div class="app-container">
         <div class="box-show">
             <el-table height="400">
                 <el-table-column label="test"></el-table-column>
@@ -16,6 +16,31 @@
                 <el-table-column label="test"></el-table-column>
                 <el-table-column label="test"></el-table-column>
             </el-table>
+            
         </div>
+    </div> -->
+    <div style="width:400px;height:400px;">
+         <PicZoom  :url="baseUrl+avatar" :scale="3"/>
     </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    components: {
+        PicZoom : () => import('@/components/zoom/PicZoom.vue')
+    },
+    data() {
+        return {
+            baseUrl: MYURL.CUSTOMER_SERVER,
+        }
+    },
+      computed:{
+            ...mapGetters([
+            'sidebar',
+            'avatar',
+            'name',
+            ])
+        },
+}
+</script>
