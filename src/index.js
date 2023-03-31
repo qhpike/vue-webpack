becc1f21c6e44b6d805ed0fc822f2184df86d76b
akexian <8739161@qq.com>
2023-03-31 18:19:22 +0800

    样式调整

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 import "normalize.css/normalize.css";
 import "@/styles/index.scss";
 import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+// import "element-ui/lib/theme-chalk/index.css";
 import VueViewer from 'v-viewer'
 import 'viewerjs/dist/viewer.css' // 引入css
 import Vue from "vue";
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const WebpackBar = require('webpackbar')
-const { DefinePlugin  } = require('webpack')
+const { DefinePlugin } = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const readEnv = require('./readEnv')
 const { resolve } = require('path')
@@ -17,69 +17,69 @@ const envirmont = readEnv()
 module.exports = env => {
   return {
     entry: {
-      main:'./src/index.js'
+      main: './src/index.js'
     },
     output: {
-      path:path.resolve(__dirname,'dist'),
-      filename:'static/js/[name].[contenthash:6].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'static/js/[name].[contenthash:6].bundle.js',
       chunkFilename: 'static/js/[name].[chunkhash:6].chunk.js',
-      assetModuleFilename:'static/[name].[contenthash].[ext]',
-      clean:true,
+      assetModuleFilename: 'static/[name].[contenthash].[ext]',
+      clean: true,
     },
-    mode:env.development ? 'development' : 'production' ,
+    mode: env.development ? 'development' : 'production',
     devtool: env.development ? 'source-map' : false,
-    devServer:{
-      port:90,
-      static:'./dist',
-      hot:true,
+    devServer: {
+      port: 90,
+      static: './dist',
+      hot: true,
     },
-    plugins:[
+    plugins: [
       new HtmlWebpackPlugin({
-        title:'my-web-test',
-        template:'./public/index.html'
+        title: 'my-web-test',
+        template: './public/index.html'
       }),
       new MiniCssExtractPlugin({
-        filename:'static/css/[name].[contenthash].css',
+        filename: 'static/css/[name].[contenthash].css',
         // chunkFilename: 'static/cssextract/[id].[contenthash].css'
       }),
       new DefinePlugin({
-        BASEURL:"'./'",
-        MYURL :JSON.stringify({
+        BASEURL: "'./'",
+        MYURL: JSON.stringify({
           ...envirmont
         })
-        
+
       }),
       new VueLoaderPlugin(),
       new CopyWebpackPlugin(
         {
-          patterns:[
-            {from:'./public/favicon.ico'},
+          patterns: [
+            { from: './public/favicon.ico' },
           ]
         }
       ),
       new WebpackBar({
         color: env.development ? "#00BFFF" : "#67c23a",  // 默认green，进度条颜色支持HEX
         basic: false,   // 默认true，启用一个简单的日志报告器
-        profile:false,  // 默认false，启用探查器。
+        profile: false,  // 默认false，启用探查器。
       }),
       new BundleAnalyzerPlugin({
-        analyzerMode: env.report ? 'server' :'disabled', //脚本后面跟reprot可开启analyzer分析
+        analyzerMode: env.report ? 'server' : 'disabled', //脚本后面跟reprot可开启analyzer分析
         // generateStatsFile: env.report
       })
     ],
     module: {
-      rules:[
+      rules: [
         {
-          test:/\.css$/i,
-          use:[
+          test: /\.css$/i,
+          use: [
             {
               loader: env.development ? 'style-loader' : MiniCssExtractPlugin.loader,
             },
             {
-              loader:'css-loader',
-              options:{
-                importLoaders:1,
-                esModule:false,
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                esModule: false,
               }
             },
             'postcss-loader'
@@ -87,25 +87,25 @@ module.exports = env => {
           // exclude:path.resolve(__dirname,'./node_modules/element-ui')
         },
         {
-          test:/\.scss$/i,
-          use:[
-            
+          test: /\.scss$/i,
+          use: [
+
             {
               loader: env.development ? 'style-loader' : MiniCssExtractPlugin.loader,
             },
             {
-              loader:'css-loader',
-              options:{
-                importLoaders:2,
-                esModule:false,
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                esModule: false,
               }
             },
             'postcss-loader',
             'sass-loader',
             {
-              loader:'sass-resources-loader',
-              options:{
-                resources:'./src/styles/var.module.scss'
+              loader: 'sass-resources-loader',
+              options: {
+                resources: './src/styles/var.module.scss'
               }
             }
           ]
@@ -116,7 +116,7 @@ module.exports = env => {
           generator: {
             filename: 'static/img/[name].[contenthash:6][ext]'
           },
-          exclude:resolve('src/icons/svg')
+          exclude: resolve('src/icons/svg')
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/i,
@@ -126,28 +126,28 @@ module.exports = env => {
           }
         },
         {
-          test:/\.js$/i,
-          loader:'babel-loader',
-          exclude:/node_modules/
+          test: /\.js$/i,
+          loader: 'babel-loader',
+          exclude: /node_modules/
         },
         {
-          test:/\.svg$/i,
-          use:[
+          test: /\.svg$/i,
+          use: [
             {
-              loader:'svg-sprite-loader',
-              options:{
+              loader: 'svg-sprite-loader',
+              options: {
                 symbolId: 'icon-[name]',
               }
             },
           ],
-          include:resolve('src/icons/svg')
+          include: resolve('src/icons/svg')
         },
         {
-          test:/\.vue$/i,
-          loader:'vue-loader',
-          options:{
-            loaders:[
-            
+          test: /\.vue$/i,
+          loader: 'vue-loader',
+          options: {
+            loaders: [
+
               {
                 loader: env.development ? 'style-loader' : MiniCssExtractPlugin.loader,
               },
@@ -174,48 +174,49 @@ module.exports = env => {
     optimization: {
       moduleIds: 'deterministic',
       runtimeChunk: 'single',
-     splitChunks: {
-       cacheGroups: {
-         vendor: {
-           test: /[\\/]node_modules[\\/]/,
-           name: 'vendors',
-           chunks: 'all',
-         },
-         element: {
-           test: /[\\/]node_modules[\\/]element-ui[\\/]/,
-           name: 'element',
-           chunks: 'all',
-           priority:10
-         },
-         vue: {
-           test: /[\\/]node_modules[\\/]vue[\\/]/,
-           name: 'vue',
-           chunks: 'all',
-           priority:10
-         },
-         vuex: {
-          test: /[\\/]node_modules[\\/]vuex[\\/]/,
-          name: 'vuex',
-          chunks: 'all',
-          priority:10
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+          element: {
+            test: /[\\/]node_modules[\\/]element-ui[\\/]/,
+            name: 'element',
+            chunks: 'all',
+            priority: 10
+          },
+          vue: {
+            test: /[\\/]node_modules[\\/]vue[\\/]/,
+            name: 'vue',
+            chunks: 'all',
+            priority: 10
+          },
+          vuex: {
+            test: /[\\/]node_modules[\\/]vuex[\\/]/,
+            name: 'vuex',
+            chunks: 'all',
+            priority: 10
+          },
         },
-       },
-     },
+      },
     },
     performance: {
       hints: "warning", // 枚举
       maxAssetSize: 20000000, // 整数类型（以字节为单位）
       maxEntrypointSize: 30000000, // 整数类型（以字节为单位）
     },
-    resolve:{
+    resolve: {
       fallback: {
-        path:require.resolve('path-browserify')
+        path: require.resolve('path-browserify')
       },
-      alias:{
-        '@':path.resolve(__dirname,'src/')
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+        'element-ui': path.resolve(__dirname, 'node_modules/element-ui')
       },
-      extensions: [".js", ".json", ".jsx", ".css",".vue"],
+      extensions: [".js", ".json", ".jsx", ".css", ".vue"],
     },
-    stats:'all'
+    stats: 'all'
   }
 }
